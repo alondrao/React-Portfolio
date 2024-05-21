187c560c57c05e0878a40903a1f5a6c33bc752c6
pebbbles <alondra@Kiaras-MBP.routerlogin.net>
2024-05-21 00:06:51 -0400

    header and home js edited

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section id="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>Hi, I'm [Kiara Alondra Ovando]. I'm a [Full-Stack Web Developer].</p>
+    </section>
+  );
+};
+
+export default Home;
